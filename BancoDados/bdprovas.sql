da339b433be02ed3d93215608f9e20c8c17234c2
Diego Jonatan de Miranda <103201121+Djonatan01@users.noreply.github.com>
2022-11-09 11:39:39 -0300

    Beta 1.0.1.1
    Atualização do banco de dados

--- BancoDados/bdprovas.sql
+++ BancoDados/bdprovas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 05-Nov-2022 às 19:50
+-- Tempo de geração: 09-Nov-2022 às 15:38
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 7.4.29
 
@@ -35,6 +35,52 @@ CREATE TABLE `eventos` (
   `periodo` varchar(10) NOT NULL,
   `curso` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `senha` varchar(10) NOT NULL,
+  `matricula` int(14) NOT NULL,
+  `tipo` varchar(50) NOT NULL,
+  `periodo` varchar(10) NOT NULL,
+  `curso` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `matricula`, `tipo`, `periodo`, `curso`) VALUES
+(1, 'Diego Jonatan', 'diego.miranda@fatec.sp.gov.com', '123456', 1314182921, 'Aluno', '3° semestr', 'ADS'),
+(2, 'Lucas Adati de Paula', 'Lucas@adati.com', '123', 65478914, 'Aluno', '3° semestr', 'ADS'),
+(3, 'qq', 'qqq', '111', 111, 'aaa', 'qq', 'qqq');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
