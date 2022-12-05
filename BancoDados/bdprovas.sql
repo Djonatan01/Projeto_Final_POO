227415c600c5f6d8abcea00a9ce0ddb5a8f33f9b
Diego Jonatan de Miranda <103201121+Djonatan01@users.noreply.github.com>
2022-12-05 20:12:17 -0300

    Update bdprovas.sql

--- BancoDados/bdprovas.sql
+++ BancoDados/bdprovas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 26-Nov-2022 às 23:13
+-- Tempo de geração: 05-Dez-2022 às 22:25
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 7.4.29
 
@@ -45,8 +45,10 @@ CREATE TABLE `eventos` (
 INSERT INTO `eventos` (`id`, `data`, `hora`, `evento`, `disciplina`, `periodo`, `curso`, `sala`) VALUES
 (4, '29/11/2022', '10:35', 'Prova de engenharia de softwar', 'Engenharia de software', '3° Semestr', 'ADS', 'L4'),
 (6, '30/11/2022', '08:15', 'Apresentação', 'Sistemas Operacionais', '3° Semestr', 'GPI', 'L3'),
-(7, '30/11/2022', '07:00', 'Apresentação', 'Sistemas Operacionais', '3° Semestr', 'GPI', 'L3'),
-(9, '09/12/2022', '07:00', 'ttttt', 'Estrutura de dados', '3° Semestr', 'ADS', 'L3');
+(7, '30/11/2022', '07:00', 'Apresentação 2', 'Sistemas Operacionais', '3° Semestr', 'ADS', 'L3'),
+(10, '30/11/2022', '07:50', 'Prova', 'Inglês', '2° Semestr', 'EVENT', 'L2'),
+(11, '30/11/2022', '10:45', 'teste', 'teste', '1° Semestr', 'GPI', 'L1'),
+(12, '30/11/2022', '10:30', 'prova', 'POO', '3° Semestr', 'ADS', 'L3');
 
 -- --------------------------------------------------------
 
@@ -59,7 +61,7 @@ CREATE TABLE `usuarios` (
   `nome` varchar(100) NOT NULL,
   `email` varchar(200) NOT NULL,
   `senha` varchar(10) NOT NULL,
-  `matricula` int(14) NOT NULL,
+  `matricula` varchar(14) NOT NULL,
   `tipo` varchar(50) NOT NULL,
   `periodo` varchar(10) NOT NULL,
   `curso` varchar(5) NOT NULL
@@ -70,12 +72,16 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `matricula`, `tipo`, `periodo`, `curso`) VALUES
-(1, 'Diego Jonatan de Miranda da silva ramos', 'teste@teste.com', '123', 15161318, 'Aluno', '3° semestr', 'ADS'),
-(2, 'Rafaela', 'rafael@teste.com', '1234', 65478914, 'Professor', '', 'GPI'),
-(3, 'Admin', 'admin', 'admin', 0, 'admin', '', ''),
-(4, 'Gabriel', 'gabriel@gabriel.com', '123', 14253663, 'aluno', '3° Semestr', 'ads'),
-(7, 'Diego da silva\r\n', '321231', '321231', 1231231, 'Professor', '1° Semestr', 'GPI'),
-(9, 'Diego silva Junior', 'hhhhhhhhh', 'hhhhhhhhh', 21564564, 'Aluno', '2° Semestr', 'EVENT');
+(1, 'Diego Jonatan de Miranda da silva ramos', 'teste@teste.com', '123', '15161318', 'Aluno', '3° semestr', 'ADS'),
+(2, 'Rafaela', 'rafael@teste.com', '1234', '65478914', 'Professor', '', 'GPI'),
+(3, 'Admin', 'admin', 'admin', '0', 'admin', '', ''),
+(4, 'Gabriel', 'gabriel@gabriel.com', '123', '14253663', 'aluno', '3° Semestr', 'ads'),
+(7, 'Diego da silva\r\n', 'die@teste.com', '321231', '1231231', 'Professor', '1° Semestr', 'GPI'),
+(9, 'Diego silva Junior', 'diego@teste.com', 'hhhhhhhhh', '21564564', 'Aluno', '2° Semestr', 'EVENT'),
+(16, 'adminn', 'adminn', 'adminn', '142536', 'Aluno', '1° Semestr', 'EVENT'),
+(17, 'admin3', 'admin3', 'admin3', '000000000000', 'Professor', '', 'GPI'),
+(20, 'aluno', 'aluno@fatec.com', '123', '111222111222', 'Aluno', '4° Semestr', 'ADS'),
+(21, 'professor', 'prof@fatec.com', '123', '111222333333', 'Professor', '', 'ADS');
 
 --
 -- Índices para tabelas despejadas
@@ -102,13 +108,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `eventos`
 --
 ALTER TABLE `eventos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
